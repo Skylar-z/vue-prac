48bac4157f34a14e999b9c487f90df43a0aebd5f
skylar-z <3581095664@qq.com>
2018-12-03 18:29:34 +0800

    1203-2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app" class="app-container">
    <mt-header fixed title="固定在顶部"></mt-header>
+
+
    123123
+
+
 
     <mt-tabbar v-model="selected">
       <mt-tab-item id="外卖">
-        <img slot="icon" src="../assets/logo.png">
+        <img slot="icon" src="./assets/logo.png">
         外卖
       </mt-tab-item>
       <mt-tab-item id="订单">
-        <img slot="icon" src="../assets/logo.png">
+        <img slot="icon" src="./assets/logo.png">
         订单
       </mt-tab-item>
       <mt-tab-item id="发现">
-        <img slot="icon" src="../assets/logo.png">
+        <img slot="icon" src="./assets/logo.png">
         发现
       </mt-tab-item>
       <mt-tab-item id="我的">
-        <img slot="icon" src="../assets/logo.png">
+        <img slot="icon" src="./assets/logo.png">
         我的
       </mt-tab-item>
     </mt-tabbar>
